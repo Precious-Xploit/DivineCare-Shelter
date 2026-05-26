a185725dbfddf394f1cb93b5ae8d18f8cc077eba
Precious-Xploit <preciouxxploit@gmail.com>
2026-05-26 20:55:18 +0100

    Form Update

---
+++ Assets/js/contact.js
@@ -0,0 +1,28 @@
+(function(){
+    const contactForm = document.querySelector('#contactForm');
+    const businessEmail = 'go@gmail.com'
+
+    
+    contactForm.addEventListener('submit', (e)=>{
+
+        const firstName = document.querySelector('.input-first-name');
+        const lastName = document.querySelector('.input-last-name');
+        const subject = document.querySelector('.input-subject');
+        const email = document.querySelector('.input-email');
+        const phoneNumber = document.querySelector('.input-phone-number');
+        const message = document.querySelector('.input-message');
+        const formattedSubject = `DCS: ${subject.value?.replaceAll(' ','%20')}`
+
+        const messageBody = `
+            ${message.value}
+
+            %0A %0A ${firstName.value} ${lastName.value}
+            %0A ${phoneNumber.value}
+            %0A ${email.value}
+        `
+
+        location.href = `mailto:${businessEmail}?subject=${formattedSubject}&body=${messageBody}`
+
+        e.preventDefault()
+    })
+})()
